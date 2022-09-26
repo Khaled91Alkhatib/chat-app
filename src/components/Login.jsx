4adf499073d9d179111450bbd6b6967e309bd74c
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-09-26 19:41:43 -0400

    Create login component to handle home page of the app

---
+++ src/components/Login.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
+import "../Styles/Login.scss";
+
+const Login = () => {
+  return (
+    <div className='background'>
+      <div className='login'>
+        <div>
+          <div className='welcome'>Worldnect!</div>
+          <div className='message'>Keep in touch with the world!</div>
+          <div className='buttons'>
+            <button className='google-facebook google'>
+              <GoogleOutlined className='icon' />
+              Sign In with Google
+            </button>
+            <button className='google-facebook'>
+              <FacebookOutlined className='icon' />
+              Sign In with Facebook
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
