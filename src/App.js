aa69476c9b940d7db586fad434d0366a5f21f13f
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-09-26 19:43:46 -0400

    Import necessary code for routing and implement code to reroute to login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./components/Login";
+
 function App() {
   return (
     <div>
-      khaled
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
