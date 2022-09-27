1fa825f39276f62bedceca939f246eb883b9b777
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-09-27 17:40:04 -0400

    Add AuthProviderand new route to Chats

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import Chats from "./components/Chats";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="/chats" element={<Chats />} />
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </div>
   );
